3cb79da3ca438676426307b3506a50eab5349896
metalfatigue <htavenido@gmail.com>
2014-04-22 01:18:28 +0800

    removing old code

--- tedious.rb
+++ tedious.rb
@@ -1,30 +1,4 @@
 module Tedious
   class LanWan
-    attr_reader :lw_mech, :page
-    def initialize url = 'http://127.0.0.1'
-      @lw_mech = Mechanize.new
-      ua_ie
-      unless content_type_set?
-        @lw_mech.post_connect_hooks << method(:force_media_type_content_html)
-      end
-      retrieve
-    end 
-
-    def retrieve
-      @lw_mech.get(@url)
-    end
-
-    def content_type_set?
-      header = retrieve.header
-      header.has_key? 'content-type' || header.content_type.empty?
-    end
-
-    def force_media_type_content_html agent, uri, response, body
-      response.content_type = 'text/html'
-    end
-  
-    def ua_ie
-      @lw_mech.user_agent = 'Windows IE 11 (Windows 8.1 64bit)'
-    end
   end
 end
--- test/test_lanwan.rb
+++ test/test_lanwan.rb
@@ -1,26 +1,4 @@
-require 'minitest/autorun'
-require 'mechanize'
 require_relative '../tedious'
 
-class TestLanWan < Minitest::Unit::TestCase
-  def setup
-    @url = 'http://192.168.254.254'
-    @lanwan = Tedious::LanWan.new @url
-  end
-
-  def test_useragent_ie
-    assert_equal 1, @lanwan.agent.user_agent
-  end
-  def test_page_as_mechpage_instance
-    assert_equal 1, @lanwan.agent.post_connect_hooks.length
-    assert_instance_of Mechanize::Page, @lanwan.agent.page, 'should be an instance of Mechanize::Page'
-  end 
-
-  def test_retrieve
-    assert_instance_of Mechanize::Page || Mechanize::File, @lanwan.retrieve
-  end
-
-  def test_content_type_present
-    assert_equal 'text/html', @lanwan.agent.page.header['content-type']
-  end
+class TestLanWan < Test::Unit::TestCase
 end
