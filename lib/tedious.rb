26fd13199bdbad5754b3a74ac07f1d703d0c36b8
metalfatigue <htavenido@gmail.com>
2014-04-27 05:55:41 +0800

    initial config format, using dotenv

--- lib/tedious.rb
+++ lib/tedious.rb
@@ -1,12 +1,24 @@
-require 'net/http'
+require 'net/http' 
 require 'uri'
+require 'dotenv'
+
+Dotenv.load
+
 module Tedious
   class Router
     def reboot
+      uri = URI.parse ENV['ROUTER_URL']
+      query = {
+        "reboot" => ENV['QPARAM_REBOOT'],
+        "encap" => ENV['QPARAM_ENCAP'],
+        "username" => ENV['QPARAM_USERNAME'],
+        "password" => ENV['QPARAM_PASSWORD'],
+        "submit-url" => ENV['QPARAM_SUBMIT_URL']
+      }
       http = Net::HTTP.new uri.host
       request = Net::HTTP::Post.new uri.path
-      request.set_form_data({
-      })
+      request.set_form_data(query)
+      request.basic_auth ENV['BASIC_AUTH_USERNAME'], ENV['BASIC_AUTH_PASSWORD']
       http.request(request)
     end
 
