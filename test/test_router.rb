ecdcc666dd074c1a9c315b5f71454f2773264c81
metalfatigue <htavenido@gmail.com>
2014-05-18 21:21:26 +0800

    outside in approach to follow, since I know already the inputs that I will be needing

--- test/test_router.rb
+++ test/test_router.rb
@@ -4,19 +4,6 @@ require_relative 'utils'
 
 describe 'router' do
   before do
-    @server = 
+    @router = Tedious::Network::Globe.new
   end
-  describe "#stats" do
-    it "should have a downstream key" do
-      router = Tedious::Network::Globe.new @url
-      router.stats.wont_be_empty 
-    end
-  end
-  describe "#retrieve_page" do
-    router = Tedious::Network::Globe.new @url
-    # given a router ip address
-    # when the index page is retrieved
-    # then it should have the current stats of the router" 
-  end
-
 end
