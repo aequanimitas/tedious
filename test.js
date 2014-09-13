ebe6db4811e7123aa745b52a71dfdbce7258a2c8
metalfatigue <htavenido@gmail.com>
2014-09-13 15:48:03 +0800

    shifting to JS

---
+++ test.js
@@ -0,0 +1,26 @@
+var http = require("http"),
+    fs   = require("fs"),
+    querystring = require("querystring"),
+    file = __dirname + "/settings.json";
+
+var form_data = querystring.stringify({
+  "rebootMode" :  "0", "reboot" :  "Reboot", "submit-url" :  "reboot.asp"
+}); 
+
+var options = {
+  host: "192.168.254.254",
+  path: "/goform/admin/formReboot",
+  method: "POST",
+  headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(form_data)
+    }  
+};
+
+fs.readFile(file, function (err,data) {
+  var data = JSON.parse(data);
+  options.auth = data.username + ":" + data.password;
+  var req = http.request(options, function(res) {});
+  req.write(form_data);
+  req.end();
+});
