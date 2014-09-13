284e00d7df366397117e49294975758e0948ffbe
metalfatigue <htavenido@gmail.com>
2014-09-13 16:27:00 +0800

    initial fuzzy logic on new format

--- tedious.js
+++ tedious.js
@@ -1,31 +1,46 @@
-var http = require("http"),
-    fs   = require("fs"),
-    querystring = require("querystring"),
-    file = __dirname + "/settings.json";
+var http = require('http'),
+    fs   = require('fs'),
+    querystring = require('querystring'),
+    file = __dirname + '/settings.json';
 
 var options = {
-  host: "192.168.254.254",
-  path: "/goform/admin/formReboot",
-  method: "POST",
+  host: '192.168.254.254',
+  path: '',
   headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(form_data)
-    }  
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': ''
+  }  
 };
 
 fs.readFile(file, function (err,data) {
   var data = JSON.parse(data);
-  options.auth = data.username + ":" + data.password;
+  options.auth = data.username + ':' + data.password;
 });
 
 
 // make this a callback fn
 function reboot() {
   var form_data = querystring.stringify({
-    "rebootMode" :  "0", "reboot" :  "Reboot", "submit-url" :  "reboot.asp"
+    'rebootMode' :  '0', 'reboot' :  'Reboot', 'submit-url' :  'reboot.asp'
   }); 
   var req = http.request(options, function(res) {});
   req.write(form_data);
   req.end();
 }
 
+function Route(path, fn) {
+  this.path = path;
+  this.fn = fn;
+}
+
+var available_routes = {
+  "reboot": new Route(
+    '/goform/admin/formReboot',
+    reboot
+  )
+}
+
+console.log(available_routes);
+http.request(options,function(res) {
+  console.log(res);
+}).end();
