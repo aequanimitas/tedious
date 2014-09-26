a097fd7b402c87853818bb039b7027400c1426b8
metalfatigue <htavenido@gmail.com>
2014-09-26 11:14:42 +0800

    basic page get

--- tedious.js
+++ tedious.js
@@ -29,7 +29,19 @@ function index(path) {
     var str = '';
     res.on('data', function(chunk) {
       str += chunk;
+    });
+    res.on('end', function () {
       console.log(str);
+    });
+  }).end();
+}
+
+function devices(path) {
+  config.router.path = path;
+  http.request(config.router, function(res) {
+    var str = '';
+    res.on('data', function(chunk) {
+      str += chunk;
     });
     res.on('end', function () {
       console.log(str);
@@ -60,9 +72,9 @@ var available_routes = {
   "reboot": new Route(
     '/goform/admin/formReboot',
     reboot),
-  "root": new Route(
-    '/globe_setup_1pwn1.asp',
-    index),
+  "devices": new Route(
+    '/admin/wlstatbl.asp',
+    devices),
   "stats": new Route(
     '/adsl-statis.asp',
     stats)
@@ -70,7 +82,7 @@ var available_routes = {
 
 function argument_switch(arg) {
   if (arg == undefined || available_routes[arg] == undefined) {
-    console.log("Choices are reboot, root and stats");
+    console.log("Choices are reboot, devices and stats");
     return 0;
   } else {
     available_routes[arg].fn(available_routes[arg].path)
