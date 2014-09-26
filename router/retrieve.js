baac6e6462a170a269c3d0774fb7e9be9275d31a
metalfatigue <htavenido@gmail.com>
2014-09-26 15:04:53 +0800

    regress to use http.get since we are not passing anything for now

--- router/retrieve.js
+++ router/retrieve.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 function retrieve(options) {
-  http.request(options, function(res) {
+  http.get(options, function(res) {
     var str = '';
     res.on('data', function(chunk) {
       str += chunk;
@@ -9,7 +9,7 @@ function retrieve(options) {
     res.on('end', function () {
       console.log(str);
     });
-  }).end();
+  });
 }
 
 module.exports = retrieve;
