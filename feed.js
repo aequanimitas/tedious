a9842dea1ddd27f4182ab4f1492fa5108a7e86a5
metalfatigue <htavenido@gmail.com>
2015-01-19 06:17:23 +0800

    cleanup, move settings into new file, moved to python

--- feed.js
+++
@@ -1,9 +0,0 @@
-var Destinare = require("./destinare");
-
-var DataFeed = function(){
-  Destinare.apply(this, Array.prototype.slice.call(arguments));
-  this.format = this.prop.format || ".json";
-}
-
-DataFeed.prototype = new Destinare();
-module.exports = DataFeed;
