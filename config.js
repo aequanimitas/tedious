04355acbe0d20d2424ac3ea6dfdbf9eb66b1b410
metalfatigue <htavenido@gmail.com>
2015-08-28 02:05:50 +0800

    Merge Tedious and Working grab code

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.config = {
+exports = module.exports = {
   keepAlive: true,
   headers: {
     "Accept-Encoding": "gzip, deflate, sdch",
@@ -6,10 +6,10 @@ exports.config = {
     "DNT": 1,
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36",
   }
-  basicAuth: {
-    username: process.env.ROUTER_BASIC_USERNAME || "user",
-    password: process.env.ROUTER_BASIC_PASSWORD || "password"
-  },
-  username: process.env.ROUTER_USERNAME || "",
-  password: process.env.ROUTER_PASSWORD || ""
+//   basicAuth: {
+//     username: process.env.ROUTER_BASIC_USERNAME || "user",
+//     password: process.env.ROUTER_BASIC_PASSWORD || "password"
+//   },
+//   username: process.env.ROUTER_USERNAME || "",
+//   password: process.env.ROUTER_PASSWORD || ""
 }
--- spec/infoTableSpec.js
+++ spec/infoTableSpec.js
@@ -2,19 +2,20 @@ var cheerio = require("cheerio");
 var infotable = require("../lib/infotable");
 
 describe("Handling Table Rows", function() {
-  describe ("pairs method", function () {
+  describe ("domPair method", function () {
     it("should just return an object", function() {
-      expect(infotable.pairs()).toBeDefined();
+      expect(infotable.domPair()).toBeDefined();
+      expect(infotable.domPair()).toEqual({});
     });
     it("should return an object with key: IP Address and value: 192.168.254.254", function() {
-      var pairs = infotable.pairs(
+      var domPair = infotable.domPair(
          "<td width=40%><font size=2><b>IP Address</b></td>" +
          "<td width=60%><font size=2>192.168.254.254</td>"
       );
-      expect(Object.keys(pairs).indexOf("IP Address")).not.toEqual(-1);
-      expect(pairs["IP Address"]).toEqual("192.168.254.254");
+      expect(Object.keys(domPair).indexOf("IP Address")).not.toEqual(-1);
+      expect(domPair["IP Address"]).toEqual("192.168.254.254");
     });
-    it("should accept a HOF which will be used to figure out the pairs", function() {
+    it("should accept a HOF which will be used to figure out the domPair", function() {
       pending("Idea is still unclear");
     });
   });
