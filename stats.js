02791e87ae75066475811fcc1b834c83c146d1b5
metalfatigue <htavenido@gmail.com>
2015-09-15 16:15:27 +0800

    fix for info table

--- stats.js
+++ stats.js
@@ -3,12 +3,19 @@ var infoTable = require("./lib/infotable");
 
 var exports = module.exports = {};
 
-function statTable(dom) {
-  var statTable = cheerio.load(dom)("table");
-   console.log(statTable.children()[1]('font b'));
-//  Array.prototype.forEach.call(statTable.children(), function(e) {
-//    console.log(e.text);
-//  });
+function globeStatsPage(dom) {
+  var $statTable = cheerio.load(dom)("tr").filter(function(i, e) { 
+    if (e.attribs.hasOwnProperty('bgcolor') && e.attribs["bgcolor"] !== "#808080") {
+       return e;
+    }
+  });
+  var stats = {};
+  Array.prototype.forEach.call($statTable, function(e) {
+    var stat       = infoTable.statPair(e),
+        currentKey = Object.keys(stat)[0];
+    stats[currentKey] = stat[currentKey];
+  });
+  console.log(stats);
 }
 
-exports.stat = statTable;
+exports.stat = globeStatsPage
