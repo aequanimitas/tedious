bdf008f1f8b695337965ecb76a5665ee40bf0dd6
metalfatigue <htavenido@gmail.com>
2015-10-10 02:41:29 +0800

    Grab DOM of active clients page

--- config.js
+++ config.js
@@ -6,10 +6,5 @@ var exports = module.exports = {
       "DNT"             : 1,
       "User-Agent"      : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36",
     },
-//    basicAuth: {
-//       username: process.env.ROUTER_BASIC_USERNAME || "user",
-//       password: process.env.ROUTER_BASIC_PASSWORD || "password"
-//    },
-  //   username: process.env.ROUTER_USERNAME || "",
-  //   password: process.env.ROUTER_PASSWORD || ""
+    auth: "user:password"
 }
--- globe.js
+++ globe.js
@@ -1,12 +1,16 @@
 var http   = require("http"),
     url    = require("url"),
-    config = require("./config"),
+    config = require("./config.js.local"),
     helpers = require("./helpers"),
-    routerUrl = "http://192.168.254.254",
+    routerUrl = "http://" + config.auth + "@192.168.254.254",
     idxPage = "/globe_setup_1pwn1.asp",
+    active_client_path = "/admin/wlstatbl.asp",
     operations = {
       reboot: function() { throw new Error("Not Yet Implemented");  },
-      active_clients: function() { throw new Error("Not Yet Implemented"); },
+      active_clients: function() { 
+        helpers.extend(config, url.parse(routerUrl + active_client_path));
+        http.request(config, httpREH).end();
+      },
       stats: function() {
         http.request(routerUrl + idxPage, httpREH).end();
       }
@@ -14,12 +18,16 @@ var http   = require("http"),
 
 function httpREH(resHandler) {
   var data = "";
+  if (resHandler.statusCode === 401) {
+     throw new Error("Unauthorized, check credentials in config.js");
+  }
   resHandler
     .on("data", function(chunk) {  
       data += chunk.toString();
     })                             
     .on("end", function() {        
       helpers.stats(data);
+      console.log(data);
       console.log("Success");
     })
     .on("error", function(err) {
