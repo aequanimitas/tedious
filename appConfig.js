37156cdb011d358b4496846adc282bf59e64a985
metalfatigue <htavenido@gmail.com>
2015-10-15 11:23:50 +0800

    update appConfig and ignored files

---
+++ appConfig.js
@@ -0,0 +1,13 @@
+var exports = module.exports = {
+  client: {
+    keepAlive: true,
+    headers: {
+      "Accept-Encoding" : "gzip, deflate, sdch",
+      "Accept-Language" : "en-US,en;q=0.8",
+      "DNT"             : 1,
+      "User-Agent"      : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36",
+    },
+    auth: "user:password",
+    ip: 'localhost'
+  }
+}
