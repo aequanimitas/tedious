031cf345a8fb2809bf373e1ca80310270d84f643
metalfatigue <htavenido@gmail.com>
2015-10-25 21:40:52 +0800

    add description and use it in io

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var router = require("./router"),
     reddit = require("./reddit"),
+    packageInfo = require("./package.json"),
     subapps = {
       "router": router,
       "reddit": reddit,
     };
 
 function startTask(opts) {
-  console.log("\nTedious: For things that you do repeatedly\n");
+  console.log("\n" + packageInfo.name + ": " + packageInfo.description + "\n");
   if (opts.length == 0) {
     console.log("Available commands: " + Object.keys(subapps).join(", ") + "\n");
   } else if (opts.length == 1) {
