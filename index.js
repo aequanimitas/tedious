40f414e39195d3a89a1c45627831f3eaf70a2779
aequanimitas <htavenido@gmail.com>
2016-02-06 02:45:28 +0800

    draining abstraction

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ var vorpal = require('vorpal')(),
     appConfig = require('./appConfig.js.local'),
     helpers = require('./helpers');
 
+function routeAction(args) {
+  var action = helpers.partial(helpers.httpResponseHelper, args.helper);
+  helpers.extend(appConfig.client, args.url);
+  return {
+    config: appConfig.client,
+    action: action
+  }
+}
+
 vorpal.command('stats', 'current router stats')
   .alias('stats')
   .action(function(args, cb) {
-     var action = helpers.partial(helpers.httpResponseHelper, helpers.stats);
-     var routerUrl = appConfig.client.ip;
-     var idxPage = '/globe_setup_1pwn1.asp';
-     helpers.extend(appConfig.client, url.parse(appConfig.client.ip + idxPage));
-     http.request(appConfig.client, action).end();
+     var endpoint = url.parse(appConfig.client.ip + '/globe_setup_1pwn1.asp')
+     var temp = routeAction({ helper: helpers.stats, url: endpoint })
+     http.request(temp.config, temp.action).end();
   });
 
 vorpal.delimiter('Tedious: ').show().parse(process.argv);
