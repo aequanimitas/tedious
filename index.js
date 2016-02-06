03409799f0dfc5c76657ebe415d3ac4eae7ce8e0
aequanimitas <htavenido@gmail.com>
2016-02-07 07:51:50 +0800

    remove alias for now, confusing during interaction

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ var vorpal = require('vorpal')(),
     Route = require('./route');
 
 vorpal.command('stats', 'current router stats')
-  .alias('stats')
   .action(function(args, cb) {
      var route = Route('stats', '/globe_setup_1pwn1.asp');
      http.request(route.config, route.action).end();
   });
 
-vorpal.command('clients', 'current router stats')
-  .alias('clients')
+vorpal.command('clients', 'router active clients')
   .action(function(args, cb) {
      var route = Route('clients', '/admin/wlstatbl.asp', true);
      http.request(route.config, route.action).end();
