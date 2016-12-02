5b024c9903319e06add59cb2c7f78bf48fe29d66
aequanimitas <htavenido@gmail.com>
2016-12-02 16:27:55 +0800

    clean up for module movement

--- lib/tedious.ex
+++ lib/tedious.ex
@@ -1,29 +1,38 @@
 defmodule Tedious do
   require Logger
-  @globe_url "http://192.168.254.254/status_globe_setup1.htm"
 
   def main(args) do
-    {_, arg, _} = OptionParser.parse(args)
+    {options, arg, _} = OptionParser.parse(args)
+    Logger.info arg
     arg
     |> process
   end
 
   def process(arg) do
-    Logger.info inspect(apply(Tedious, String.to_atom(hd arg), []), pretty: true)
+    [Atom.to_string(__MODULE__), String.capitalize(hd arg)]
+    |> Module.concat
+    |> apply(:stat, [])
+    |> inspect(pretty: true)
+    |> Logger.info
   end
 
-  def router() do
-    {_, %HTTPoison.Response{body: bd}} = HTTPoison.get @globe_url
-    {data, _} = Floki.find(bd, "td") |> Enum.split(17)
-    {headers, _} = Floki.find(bd, "th") |> Enum.split(17)
-    Enum.zip(headers, data) 
-    |> Enum.map(&trios_to_pair/1)
-    |> Enum.into(%{})
-  end
 
-  defp trios_to_pair(x) do
-    {{_,_,[h]}, {_,_,[d]}} = x
-    {h, String.replace(d, "\n", "")}
-  end
+  defmodule Router do
+    @globe_url "http://192.168.254.254/status_globe_setup1.htm"
 
+    def stat() do
+      {_, %HTTPoison.Response{body: bd}} = HTTPoison.get @globe_url
+      {data, _} = Floki.find(bd, "td") |> Enum.split(17)
+      {headers, _} = Floki.find(bd, "th") |> Enum.split(17)
+      Enum.zip(headers, data) 
+      |> Enum.map(&trios_to_pair/1)
+      |> Enum.into(%{})
+    end
+
+    defp trios_to_pair(x) do
+      {{_,_,[h]}, {_,_,[d]}} = x
+      {h, String.replace(d, "\n", "")}
+    end
+  end
 end
+
