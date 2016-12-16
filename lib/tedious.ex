8f8945fa85bb4d20689224e69291ff65b7b39691
aequanimitas <htavenido@gmail.com>
2016-12-17 05:01:43 +0800

    rework

--- lib/tedious.ex
+++ lib/tedious.ex
@@ -2,16 +2,15 @@ defmodule Tedious do
   require Logger
 
   def main(args) do
-    {options, arg, _} = OptionParser.parse(args)
-    Logger.info arg
-    arg
-    |> process
+    {_, args, _} =  OptionParser.parse(args)
+    # args catch arguments without flags
+    args |> process
   end
 
   def process(arg) do
-    [Atom.to_string(__MODULE__), String.capitalize(hd arg)]
+    [Atom.to_string(__MODULE__), String.capitalize(hd(arg))]
     |> Module.concat
-    |> apply(:stat, [])
+    |> apply(String.to_existing_atom(hd(tl(arg))), [])
     |> inspect(pretty: true)
     |> Logger.info
   end
@@ -20,7 +19,7 @@ defmodule Tedious do
   defmodule Router do
     @globe_url "http://192.168.254.254/status_globe_setup1.htm"
 
-    def stat() do
+    def stat do
       {_, %HTTPoison.Response{body: bd}} = HTTPoison.get @globe_url
       {data, _} = Floki.find(bd, "td") |> Enum.split(17)
       {headers, _} = Floki.find(bd, "th") |> Enum.split(17)
@@ -32,6 +31,17 @@ defmodule Tedious do
     defp trios_to_pair(x) do
       {{_,_,[h]}, {_,_,[d]}} = x
       {h, String.replace(d, "\n", "")}
+    end
+  end
+
+  defmodule Reddit do
+    @reddit_url "http://www.reddit.com/r/"
+
+    @doc """
+      Try meta-programming here
+    """
+    def ph do
+      "Not yet implemented"
     end
   end
 end
