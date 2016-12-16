95f4c73b7853c6fd7746de8b0350e37dcef26c45
aequanimitas <htavenido@gmail.com>
2016-12-17 05:19:23 +0800

    a bit cleaner but still funky

--- lib/tedious.ex
+++ lib/tedious.ex
@@ -8,41 +8,32 @@ defmodule Tedious do
   end
 
   def process(arg) do
-    [Atom.to_string(__MODULE__), String.capitalize(hd(arg))]
-    |> Module.concat
-    |> apply(String.to_existing_atom(hd(tl(arg))), [])
-    |> inspect(pretty: true)
-    |> Logger.info
-  end
-
-
-  defmodule Router do
-    @globe_url "http://192.168.254.254/status_globe_setup1.htm"
-
-    def stat do
-      {_, %HTTPoison.Response{body: bd}} = HTTPoison.get @globe_url
-      {data, _} = Floki.find(bd, "td") |> Enum.split(17)
-      {headers, _} = Floki.find(bd, "th") |> Enum.split(17)
-      Enum.zip(headers, data) 
-      |> Enum.map(&trios_to_pair/1)
-      |> Enum.into(%{})
-    end
-
-    defp trios_to_pair(x) do
-      {{_,_,[h]}, {_,_,[d]}} = x
-      {h, String.replace(d, "\n", "")}
-    end
-  end
-
-  defmodule Reddit do
-    @reddit_url "http://www.reddit.com/r/"
-
-    @doc """
-      Try meta-programming here
-    """
-    def ph do
-      "Not yet implemented"
+    modle = [Atom.to_string(__MODULE__), String.capitalize(hd(arg))] |> Module.concat
+    case Keyword.has_key? modle.module_info[:exports], String.to_atom(hd(tl(arg))) do
+      true ->
+        apply(modle, String.to_existing_atom(hd(tl(arg))), [])
+        |> inspect(pretty: true)
+        |> Logger.info
+      false ->
+        IO.puts "unknown operation: #{hd(tl(arg))}"
     end
   end
 end
 
+defmodule Tedious.Router do
+  @globe_url "http://192.168.254.254/status_globe_setup1.htm"
+
+  def stat do
+    {_, %HTTPoison.Response{body: bd}} = HTTPoison.get @globe_url
+    {data, _} = Floki.find(bd, "td") |> Enum.split(17)
+    {headers, _} = Floki.find(bd, "th") |> Enum.split(17)
+    Enum.zip(headers, data) 
+    |> Enum.map(&trios_to_pair/1)
+    |> Enum.into(%{})
+  end
+
+  defp trios_to_pair(x) do
+    {{_,_,[h]}, {_,_,[d]}} = x
+    {h, String.replace(d, "\n", "")}
+  end
+end
