4d338dc9bd1ec6613baedc1e81bad42cd4665d42
aequanimitas <htavenido@gmail.com>
2017-03-09 01:38:09 +0800

    set to :init by default

--- lib/tedious.ex
+++ lib/tedious.ex
@@ -1,6 +1,8 @@
 defmodule Tedious do
   require Logger
 
+  # $ ./tedious yts
+  # $ ./tedious router
   def main(args) do
     {_, args, _} =  OptionParser.parse(args)
     # args catch arguments without flags
@@ -9,22 +11,17 @@ defmodule Tedious do
 
   def process(arg) do
     modle = [Atom.to_string(__MODULE__), String.capitalize(hd(arg))] |> Module.concat
-    try do
-      atm = String.to_existing_atom(hd(tl(arg)))
-      apply(modle, atm, [])
+    apply(modle, :init, [])
       |> inspect(pretty: true)
       |> Logger.info
-    rescue
-      _ in ArgumentError -> IO.puts "Operation doesn't exist"
-    end
   end
 end
 
 defmodule Tedious.Yts do
-  @url "https://yts.ag/api/v2/list_movies.json?limit=10"
+  @url "https://yts.ag/api/v2/list_movies.json?limit=10=quality=1080p"
 
   def init do
-    latest
+    latest()
     |> decode
     |> get_movies
     |> Enum.map(&Tedious.Yts.description/1)
@@ -37,6 +34,8 @@ defmodule Tedious.Yts do
 
   def description(data) do 
     %{
+      "date_added" => data["date_uploaded_unix"] |> DateTime.from_unix(),
+      "rating" => data["rating"], 
       "title" => data["title_english"]
     }
   end
@@ -53,16 +52,13 @@ end
 defmodule Tedious.Router do
   @globe_url "http://192.168.254.254/status_globe_setup1.htm"
 
-  def stat do
+  def init do
     {_, %HTTPoison.Response{body: bd}} = HTTPoison.get @globe_url
     {data, _} = Floki.find(bd, "td") |> Enum.split(17)
     {headers, _} = Floki.find(bd, "th") |> Enum.split(17)
     Enum.zip(headers, data) 
     |> Enum.map(&trios_to_pair/1)
     |> Enum.into(%{})
-  end
-
-  def get_headers(dom_elem) do
   end
 
   defp trios_to_pair(x) do
